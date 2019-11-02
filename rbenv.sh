72be37ddc3f2139a7d314b79c8fe4dcc408b3cc0
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-01 23:51:36 -0700

    initial commit

---
+++ rbenv.sh
@@ -0,0 +1,14 @@
+sudo mkdir -p $HOME/.rbenv/shims
+sudo mkdir -p $HOME/.rbenv/versions
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev
+git clone https://github.com/tpope/rbenv-aliases.git ~/.rbenv/plugins/rbenv-aliases
+rbenv alias --auto
+
+git clone https://github.com/rbenv/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
+rbenv install 2.6.3
