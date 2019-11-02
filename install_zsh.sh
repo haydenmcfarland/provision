d7558d17de0f09da08c0203f7c5b051de0c9717b
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-02 00:40:29 -0700

    update install_zsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -15,7 +15,7 @@ fi
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
 
-sudo chsh -s /usr/bin/zsh
+chsh -s $(which zsh)
 
 # create bin directory
 echo "creating $HOME/bin directory if it doesn't exist"
