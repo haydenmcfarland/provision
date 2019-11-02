03760c33a4bd323cb7a27bf6a7736c0b92607f94
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-02 00:35:41 -0700

    update scripts

--- configure_regolith.sh
+++ configure_regolith.sh
@@ -1,3 +1,10 @@
+# initialize i3 config
+echo 'creating i3 config...'
+mkdir -p ~/.config/regolith/i3
+cp /etc/regolith/i3/config ~/.config/regolith/i3/config
+
+sed -i 's/\/usr\/bin\/st/\/usr\/bin\/gnome-terminal/g' ~/.config/regolith/i3/config
+
 # theming
 echo 'performing reglolith theming...'
 cp -n /etc/regolith/styles/root ~/.Xresources-regolith
