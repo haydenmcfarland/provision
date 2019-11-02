d1ea6ebb15255e865b1fb4dc0df1bc2800338a48
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-02 01:11:31 -0700

    update scripts

--- configure_regolith.sh
+++ configure_regolith.sh
@@ -1,10 +1,14 @@
+if ! [ -f ~/.config/regolith/i3/config ]
+then
 # initialize i3 config
 echo 'creating i3 config...'
 mkdir -p ~/.config/regolith/i3
-cp /etc/regolith/i3/config ~/.config/regolith/i3/config
+I3CONFIG=~/.config/regolith/i3/config
+cp /etc/regolith/i3/config $I3CONFIG
 
-sed -i 's/\/usr\/bin\/st/\/usr\/bin\/gnome-terminal/g' ~/.config/regolith/i3/config
-
+sed -i 's/\/usr\/bin\/st/\/usr\/bin\/gnome-terminal/g' $I3CONFIG
+sed -i 's/bar {/bar {\n  mode hide/g' $I3CONFIG
 # theming
-echo 'performing reglolith theming...'
+echo 'performing regolith theming...'
 cp -n /etc/regolith/styles/root ~/.Xresources-regolith
+fi
--- install_code.sh
+++ install_code.sh
@@ -1,4 +1,4 @@
-if ! [-f '/etc/apt/sources.list.d/vscode.list']
+if ! [ -f /etc/apt/sources.list.d/vscode.list ];
 then
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
