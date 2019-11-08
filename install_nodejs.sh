b17eb20def85b48649b7473a13c78af431108352
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-07 22:27:18 -0800

    slightly better, but brittle nodejs script

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-wget https://nodejs.org/dist/v13.1.0/node-v13.1.0-linux-x64.tar.xz
-tar -xvf node-v13.1.0-linux-x64.tar.xz
-sudo rm node-v13.1.0-linux-x64.tar.xz
-sudo mv node-v13.1.0-linux-x64/bin/* /usr/local/bin
-sudo rm -R node-v13.1.0-linux-x64*
+# fetch latest version number from nodejs.org
+VERSION=$(curl -s https://nodejs.org/en/ | grep -Po '\d+\.\d\.\d' | head -1)
+FILE="node-v$VERSION-linux-x64"
+wget "https://nodejs.org/dist/v$VERSION/$FILE.tar.xz"
+tar -xvf "$FILE.tar.xz"
+sudo rm "$FILE.tar.xz"
+sudo mv $FILE/bin/* "/usr/local/bin"
+sudo rm -R $FILE*
