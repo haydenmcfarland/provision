5a3a87a797358abe4d7cecbde3a4dd75f4c347db
Hayden McFarland <hmcfarland@pnmac-c4dc35.pnmac.com>
2020-01-31 16:00:25 -0800

    add macos config variant

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Summary:
 
-A collection of provisioning scripts to set up my own PC (Ubuntu).
+A collection of provisioning scripts to set up my own PC (Ubuntu/macos).
---
+++ macos/setup.sh
@@ -0,0 +1,8 @@
+
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install packages
+brew cask install $(cat packages.txt)
+
+cp -f 'settings.json' ~/'Library/Application Support/Code/User/settings.json'
