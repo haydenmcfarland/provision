e3d9f7c971a29b72ea3bc1edb8873ddf0e6a77f7
Hayden McFarland <hmcfarland@pnmac-c4dc35.pnmac.com>
2020-01-31 17:08:35 -0800

    refactor and add git for macos

--- macos/setup.sh
+++ macos/setup.sh
@@ -3,6 +3,7 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install packages
-brew cask install $(cat packages.txt)
+brew cask install $(cat ui_packages.txt)
+brew install $(cat packages.txt)
 
 cp -f 'settings.json' ~/'Library/Application Support/Code/User/settings.json'
