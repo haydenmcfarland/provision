5115722c9dfef0360e310366cc98fdb21f09bdd7
Hayden McFarland <mcfarlandsms@gmail.com>
2020-06-02 21:52:33 -0700

    add docker-compose

--- ubuntu/install_docker.sh
+++ ubuntu/install_docker.sh
@@ -17,3 +17,5 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker ${USER}
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
