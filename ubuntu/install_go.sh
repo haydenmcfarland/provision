6719cb825f27ddefbbc3474d8f3fc329d5496482
Hayden McFarland <mcfarlandsms@gmail.com>
2020-06-04 18:31:45 -0700

    update go install script

--- ubuntu/install_go.sh
+++ ubuntu/install_go.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-curl -O https://storage.googleapis.com/golang/go1.13.4.linux-amd64.tar.gz
-tar -xvf go1.13.4.linux-amd64.tar.gz
-sudo mv go /usr/local
-sudo rm go1.13.4.linux-amd64.tar.gz
+VERSION=$(
+  curl -s https://golang.org/dl/ | grep -Po -m 1 'go1.1\d\.\d' | head -1
+)
+TARGET_FILE="$VERSION.linux-amd64.tar.gz"
+
+curl -O https://storage.googleapis.com/golang/$TARGET_FILE
+tar -xvf $TARGET_FILE
+sudo rsync -a -v go /usr/local --remove-source-files
+sudo rm $TARGET_FILE
 
 if grep -q "GOPATH" ~/.profile;
 then echo 'GOPATH already configured'
 else
-echo "export GOPATH=$HOME/go_projects" >> ~/.profile
-echo "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> ~/.profile
+  sudo mkdir -p ~/go_projects
+  echo "export GOPATH=$HOME/go_projects" >> ~/.profile
+  echo "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> ~/.profile
 fi
-
-sudo mkdir ~/go_projects
